a7d5126d5b8d689f68d3a9747729140fd9960344
Marco Tondi <marcotondi@gmail.com>
2020-03-20 14:20:55 +0100

    Updates

---
+++ precache-manifest.189eb26ca30c6f279f8901f2afb49f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408499d14a98f8622eedece0c57dcd1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa0686bea7cf814ad058",
+    "url": "/static/css/main.a2033702.chunk.css"
+  },
+  {
+    "revision": "c2c3ef08974dae92567a",
+    "url": "/static/js/2.31f0b9ee.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.31f0b9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0686bea7cf814ad058",
+    "url": "/static/js/main.9f722052.chunk.js"
+  },
+  {
+    "revision": "1b3b27500a7d5c547852",
+    "url": "/static/js/runtime-main.637e9360.js"
+  },
+  {
+    "revision": "c71ccbdb43a05f2035b3d0dea82c14d1",
+    "url": "/static/media/marco.c71ccbdb.png"
+  },
+  {
+    "revision": "5a681849009898ab8f3d02c03c8e895e",
+    "url": "/static/media/melania.5a681849.png"
+  }
+]);
